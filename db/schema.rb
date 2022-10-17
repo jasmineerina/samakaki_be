f7591eb27649ac032c7ac884a8167bd2ea6a6862
jasmineerina15 <jasmineerina15@gmail.com>
2022-10-17 13:31:08 +0700

    add create_event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_14_070058) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_053400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_14_070058) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_biodata_users_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date"
+    t.string "venue"
+    t.bigint "user_id"
+    t.bigint "family_tree_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["family_tree_id"], name: "index_events_on_family_tree_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "family_trees", force: :cascade do |t|
