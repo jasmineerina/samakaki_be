2d01115bcdf335dee8d17a1e0d2dc76272e5a0e8
DhimasWeray <dhimweray222@gmail.com>
2022-10-19 09:19:07 +0700

    change host to cloudinary

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  ActiveStorage::Current.host = "https://sama-kaki.herokuapp.com/"
+  ActiveStorage::Current.host = "http://res.cloudinary.com"
   has_one_attached :content
 end
