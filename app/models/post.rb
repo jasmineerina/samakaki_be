88031934ac74967b0e46305e6b0224d6b1b797df
DhimasWeray <dhimweray222@gmail.com>
2022-10-19 09:29:36 +0700

    change url options on model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  ActiveStorage::Current.host = "http://res.cloudinary.com"
+  ActiveStorage::Current.url_options = "http://res.cloudinary.com"
   has_one_attached :content
 end
