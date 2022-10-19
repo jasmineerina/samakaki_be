6b701139d814e803e0607e9ff3a1856219e00d13
DhimasWeray <dhimweray222@gmail.com>
2022-10-19 10:22:37 +0700

    add  config.active_storage.service = :cloudinary test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  ActiveStorage::Current.url_options
   has_one_attached :content
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
 
   # Store uploaded files on the local file system in a temporary directory.
-  config.active_storage.service = :test
+  config.active_storage.service = :cloudinary
 
   config.action_mailer.perform_caching = false
 
@@ -54,6 +54,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
+
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
