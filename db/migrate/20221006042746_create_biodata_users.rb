3683fb7d8ef85d51bb0c5d90c077d47bec0cc301
DhimasWeray <dhimweray222@gmail.com>
2022-10-24 09:25:46 +0700

    solve trouble

--- db/migrate/20221006042746_create_biodata_users.rb
+++ db/migrate/20221006042746_create_biodata_users.rb
@@ -5,8 +5,9 @@ class CreateBiodataUsers < ActiveRecord::Migration[7.0]
       t.string :address
       t.string :marriage_status
       t.string :status
-
+      t.references :user, index: true
       t.timestamps
     end
+    add_foreign_key :biodata_users, :users
   end
 end
