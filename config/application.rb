8f5d1c50916c6272f6e40443a8994981071c5f5d
DhimasWeray <dhimweray222@gmail.com>
2022-10-26 13:49:12 +0700

    add config cors

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,10 @@ module SamaKaki
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+
+        resource "*",
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
   end
