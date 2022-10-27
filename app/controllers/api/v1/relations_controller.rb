37269f0070d9f0f111ef24571738e5e9d0c5bca3
DhimasWeray <dhimweray222@gmail.com>
2022-10-27 11:29:16 +0700

    refactor response relation and solve problems

--- app/controllers/api/v1/relations_controller.rb
+++ app/controllers/api/v1/relations_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RelationsController < ApplicationController
   before_action :authorize, only: [:create, :update]
 
   def create
-    @relation = Relation.new(relation_params.merge(user_id:@user.id,family_tree_id: params["family_tree_id"],connected_user_id:params["connected_user_id"]))
+    @relation = Relation.new(relation_params.merge(user_id:@user.id,family_tree_id: params["family_tree_id"],connected_user_id:params["connected_user_id"],status:params["status"]))
     @relation.save ? response_to_json({relation:@relation.get_relation_from_invitation, user_relation: @relation.user_relations}, :success) : response_error(@relation.errors, :unprocessable_entity)
   end
 
--- app/controllers/api/v1/user_relations_controller.rb
+++ app/controllers/api/v1/user_relations_controller.rb
@@ -1,29 +1,28 @@
 class Api::V1::UserRelationsController < ApplicationController
   before_action :authorize, only: [:index,:show]
+  before_action do
+    ActiveStorage::Current.url_options = { protocol: request.protocol, host: request.host, port: request.port }
+  end
     def index
-      @relations = UserRelation.where(user_id: @user.id).where.not(connected_user_id: nil)
+      @relations = UserRelation.where(user_id: @user.id)
       @relation_detail =[]
       @user=[]
       @relations.each_with_index do |relation, index|
-        @user = User.find_by_id(relation.connected_user_id)
-        @relation_detail.push(
-          {
-            user_relation:relation,relation:relation.relation,biodata:
-            {
-              biodata:@user.biodata_user
-            }
-          }
-        )
+        if relation.connected_user_id==nil
+          @relation_detail.push(relation:{relation_user:relation.relation})
+        else
+          @user = User.find_by_id(relation.connected_user_id)
+          @relation_detail.push(relation:{relation_user:relation.relation,user: @user,biodata:@user.biodata_user,avatar:@user.biodata_user.avatar.url})
+        end
       end
-      response_to_json({relations: @relations},:success)
+      response_to_json({relations: @relation_detail},:success)
     end
 
     def show
       @relation = UserRelation.find_by_id(params[:id])
       @user = User.find_by_id(@relation.connected_user_id)
-      response_to_json({relation: @relation,user:@user,biodata:@user.biodata_user},status: :success)
+      response_to_json({relation: @relation.relation,user:@user,biodata:@user.biodata_user,avatar:@user.biodata_user.avatar.url},status: :success)
     end
 end
+# .where.not(connected_user_id: nil)
 
-
-#  avatar: @user.biodata_user.avatar.url
