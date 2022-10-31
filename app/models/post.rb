56076d4171c9eebb3da9bd649f7498ce826d2fd6
jasmineerina15 <jasmineerina15@gmail.com>
2022-10-31 11:53:23 +0700

    add validation image size

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :content
+  validate :acceptable_image
+
   def new_attribute
     {
       id: self.id,
@@ -8,5 +10,17 @@ class Post < ApplicationRecord
       status: self.status,
       content: self.content.url,
     }
+  end
+
+  private
+
+  def acceptable_image
+    unless content.byte_size <= 1.megabyte
+      errors.add(:content, "is too big")
+    end
+    acceptable_types = ["image/jpeg", "image/png", "image/gif"]
+    unless acceptable_types.include?(content.content_type)
+      errors.add(:content, "must be a JPEG, PNG or GIF")
+    end
   end
 end
