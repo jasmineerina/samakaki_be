36be0ee429bba57603cb4e3453389476b81bb473
DhimasWeray <dhimweray222@gmail.com>
2022-11-01 19:48:03 +0700

    remove code on connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,20 +1,4 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    before_action :authorize, only: [:connect]
-
-    def connect
-      self.current_user = find_verified_user
-    end
-
-    private
-      def find_verified_user
-        # or however you want to verify the user on your system
-        if @user
-          @user
-        else
-          reject_unauthorized_connection
-        end
-      end
-  end
   end
 end
