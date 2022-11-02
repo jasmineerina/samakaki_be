faceef5f192d24e39e37c982537789076338b8ea
DhimasWeray <dhimweray222@gmail.com>
2022-11-02 13:25:41 +0700

    add relation on model chat management

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   validates :email, :presence => true, :uniqueness => true
   has_many :user_relations, dependent: :destroy
   has_many :notifications
+  has_many :messages
+  has_many :pasticipants
   include BCrypt
   validates :email, email: {domain: 'gmail.com'}
 
