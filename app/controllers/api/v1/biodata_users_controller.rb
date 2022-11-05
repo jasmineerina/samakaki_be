11aebf71214a348f10ce1c4f4a61d84652d90675
DhimasWeray <dhimweray222@gmail.com>
2022-11-05 22:17:58 +0700

    add update user on update biodata

--- app/controllers/api/v1/biodata_users_controller.rb
+++ app/controllers/api/v1/biodata_users_controller.rb
@@ -15,7 +15,12 @@ class Api::V1::BiodataUsersController < ApplicationController
     end
 
     def update
-        @biodata.update(biodata_params) ? response_to_json(@biodata.new_attribute, :success) : response_error(@biodata.errors, :unprocessable_entity)
+        if params[:name] || params[:phone]
+            @user_detail.update(user_params)
+            @biodata.update(biodata_params) ? response_to_json(@biodata.new_attribute, :success) : response_error(@biodata.errors, :unprocessable_entity)
+        else
+            @biodata.update(biodata_params) ? response_to_json(@biodata.new_attribute, :success) : response_error(@biodata.errors, :unprocessable_entity)
+        end
     end
 
     private
@@ -24,9 +29,15 @@ class Api::V1::BiodataUsersController < ApplicationController
         params.permit(:email, :dob, :address, :marriage_status, :status, :avatar)
     end
 
+    def user_params
+        params.permit(:name,:phone)
+    end
+
     def set_biodata
+        @user_detail = User.find_by_id(@user.id)
+        response_error("user not found", :not_found) unless @user_detail.presence
         @biodata = BiodataUser.find_by(user_id: @user.id)
-        response_error("biodata not found", :not_found) unless @biodata.presence
+        response_error("user not found", :not_found) unless @biodata.presence
     end
 
 end
