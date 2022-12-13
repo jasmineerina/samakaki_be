89861ac7f3a4c1db89ee0804166c73c93775c162
DhimasWeray <dhimweray222@gmail.com>
2022-12-13 14:15:44 +0700

    try deploy rails admin again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.assets.precompile += %w(rails_admin.css rails_admin.js)
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -51,7 +52,7 @@ Rails.application.configure do
     enable_starttls_auto: true,
     open_timeout:         5,
     read_timeout:         5 }
-  
+
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
