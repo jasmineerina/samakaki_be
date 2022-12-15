786fb7d41c061c310e156edb1b478dc6652a0392
DhimasWeray <dhimweray222@gmail.com>
2022-12-15 12:19:16 +0700

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :messages
   has_many :participants
   include BCrypt
-  validates :email, email: {domain: 'gmail.com'}
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
   before_create :confirmation_token
   def password
     @password ||= Password.new(password_digest)
