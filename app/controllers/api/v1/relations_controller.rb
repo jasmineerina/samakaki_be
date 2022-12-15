a5777f12cc89b7513f6706f26ff96e2bb6ab8150
DhimasWeray <dhimweray222@gmail.com>
2022-12-15 22:31:40 +0700

    update message on notification

--- app/controllers/api/v1/relations_controller.rb
+++ app/controllers/api/v1/relations_controller.rb
@@ -15,14 +15,14 @@ class Api::V1::RelationsController < ApplicationController
 
   def update
     @relation = Relation.find(params[:id])
-    @relation.update(relation_params) ? response_to_json({relation: @relation,message:"berhasil update relation"}, :success) : response_error(@relation.errors,:unprocessable_entity)
+    @relation.update(relation_params) ? response_to_json({relation: @relation,message:"berhasil memperbarui hubungan"}, :success) : response_error(@relation.errors,:unprocessable_entity)
   end
 
   def create_notif_invited_user
     begin
       @token= JWT.decode(params[:token_invitation], SECRET_KEY)[0]
       @relation = Relation.find_by_id(@token["relation_id"])
-      @notif = Notification.find_or_create_by(user_relation_id:@relation.user_relations[0].id,user_id:@user.id,status:0,descriptions:"Anda di invit ke dalam family tree oleh #{@relation.user_relations[0].user.name}, apakah anda mengenal #{@relation.user_relations[0].user.name}?")
+      @notif = Notification.find_or_create_by(user_relation_id:@relation.user_relations[0].id,user_id:@user.id,status:0,descriptions:"Anda di undang ke dalam family tree oleh #{@relation.user_relations[0].user.name}, apakah anda mengenal #{@relation.user_relations[0].user.name}?")
       @notif ? response_to_json(@notif, :success) : response_error(@notif.errors, :unprocessable_entity)
     rescue JWT::DecodeError
       response_error("JWT TIDAK VALID",:unprocessable_entity)
