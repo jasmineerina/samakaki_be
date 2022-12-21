63f019a9bea135ebdfe5b666c2cd80efd4ec3fe1
DhimasWeray <dhimweray222@gmail.com>
2022-12-21 13:35:15 +0700

    can update notif when email not confirm

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::NotificationsController < ApplicationController
   before_action :authorize, only: [:index]
   before_action :set_notif,only: [:show, :update]
-  before_action :verif_email, only: [:update]
   before_action do
     ActiveStorage::Current.url_options = { protocol: request.protocol, host: request.host, port: request.port }
   end
